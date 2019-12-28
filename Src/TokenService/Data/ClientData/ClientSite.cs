ec67e324724c642f02827c253bde2e104dc820b4
John Melville <jmelvill@hotmail.com>
2019-12-28 10:13:12 -0700

    CreateClients

---
+++ Src/TokenService/Data/ClientData/ClientSite.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using IdentityServer4.Models;
+
+namespace TokenService.Data.ClientData
+{
+    public class ClientSite
+    {
+        public string FriendlyName { get; set; } = "";
+        public string ShortName { get; set; } = "";
+        public string ClientSecret { get; set; } = "";
+        public string BaseUri { get; set; } = "";
+        public string RedirectExtenstions { get; set; } = "signin-oidc";
+        public string FrontChannelLogoutExtension { get; set; } = "signout-oidc";
+        public string PostLogoutRedirectExtensions { get; set; } = "signout-callback-oidc";
+        public string AllowedScopes { get; set; } = "openid|profile";
+
+        public ApiResource ApiResource()
+        {
+            return new ApiResource($"api{ShortName}", FriendlyName);
+        }
+
+        public Client[] Clients()
+        {
+            return new Client[]{WebClient(), AppClient()};
+        }
+
+        private Client WebClient()
+        {    
+            return new Client
+            {
+                ClientId = $"web{ShortName}",
+                ClientName = FriendlyName,
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                ClientSecrets = new[]{new Secret(ClientSecret.Sha256())},
+                RequireClientSecret = true,
+                RedirectUris = ExpandUriExtensions(RedirectExtenstions),
+                FrontChannelLogoutUri = ExpandUriExtension(FrontChannelLogoutExtension),
+                PostLogoutRedirectUris = ExpandUriExtensions(PostLogoutRedirectExtensions),
+                AllowedScopes = BuildScopes()
+            };
+        }
+
+        private ICollection<string> BuildScopes()
+        {
+            var ret = new HashSet<string>();
+            ret.Add("openid");
+            ret.Add("profile");
+            ret.Add("apiCapp");
+            return ret;
+        }
+
+        private string ExpandUriExtension(string extension) => $"{BaseUri}/{extension}";
+
+        public IList<string> ExpandUriExtensions(string extensions) =>
+            extensions.Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .Select(i => $"{BaseUri}/{i}")
+                .ToList();
+
+        private Client AppClient()
+        {
+            return new Client
+            {
+                ClientId = $"app{ShortName}",
+                ClientName = FriendlyName,
+                ClientUri = BaseUri,
+                AllowedGrantTypes = GrantTypes.Code,
+                ClientSecrets = new[]{new Secret(ClientSecret.Sha256())},
+                RequirePkce = true,
+                RequireClientSecret = false,
+                RedirectUris = ExpandUriExtensions(RedirectExtenstions),
+                AllowedScopes = BuildScopes()
+            };
+        }
+    }
+}
---
+++ Src/TokenServiceTest/Data/ClientSiteTest.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using IdentityModel;
+using IdentityServer4.Models;
+using TokenService.Data.ClientData;
+using Xunit;
+
+namespace TokenServiceTest.Data
+{
+    public class ClientSiteTest
+    {
+        private readonly ClientSite sut = new ClientSite
+        {
+            FriendlyName = "ClientApplication",
+            ShortName = "CApp",
+            ClientSecret = "The Secret",
+            BaseUri = "https://www.Capp.Example.Com"
+        };
+
+        [Fact]
+        public void ApiResourceTest()
+        {
+            var res = sut.ApiResource();
+            Assert.Equal("apiCApp", res.Name);
+            Assert.Equal("ClientApplication", res.DisplayName);
+        }
+
+        [Fact]
+        public void Clients()
+        {
+            var clients = sut.Clients();
+            Assert.Equal(2, clients.Length);
+            Assert.True(clients.All(i=>i!=null));
+            
+        }
+
+        [Fact]
+        public void WebClientVerify()
+        {
+            var client = sut.Clients()[0];
+            Assert.Equal("webCApp", client.ClientId);
+            Assert.Equal("ClientApplication", client.ClientName);
+            Assert.Equal(GrantTypes.CodeAndClientCredentials, client.AllowedGrantTypes);
+            Assert.True(client.RequireClientSecret);
+            Assert.Equal(new[]{"The Secret".Sha256()}, client.ClientSecrets.Select(i=>i.Value));
+            Assert.Equal(new []{"https://www.Capp.Example.Com/signin-oidc"}, client.RedirectUris);
+            Assert.Equal("https://www.Capp.Example.Com/signout-oidc", client.FrontChannelLogoutUri);
+            Assert.Equal("https://www.Capp.Example.Com/signout-callback-oidc", client.PostLogoutRedirectUris.First());
+            Assert.Equal(new[] { "openid", "profile", "apiCapp" }, client.AllowedScopes);
+        }
+
+        [Fact]
+        public void ApiClientVerify()
+        {
+            var client = sut.Clients()[1];
+            Assert.Equal("appCApp", client.ClientId);
+            Assert.Equal("ClientApplication", client.ClientName);
+            Assert.Equal("https://www.Capp.Example.Com", client.ClientUri);
+            Assert.Equal(GrantTypes.Code, client.AllowedGrantTypes);
+            Assert.True(client.RequirePkce);
+            Assert.False(client.RequireClientSecret);
+            Assert.Equal(new[]{"The Secret".Sha256()}, client.ClientSecrets.Select(i=>i.Value));
+            Assert.Equal(new []{"https://www.Capp.Example.Com/signin-oidc"}, client.RedirectUris);
+            Assert.Equal(new[] { "openid", "profile", "apiCapp" }, client.AllowedScopes);
+        }
+
+        [Fact]
+        public void MultipleRedirectExtensions()
+        {
+            sut.RedirectExtenstions = "ex1|ex2/ex3";
+            Assert.Equal(new []{"https://www.Capp.Example.Com/ex1","https://www.Capp.Example.Com/ex2/ex3"}, 
+                sut.Clients()[0].RedirectUris);
+            
+        }
+    }
+}
