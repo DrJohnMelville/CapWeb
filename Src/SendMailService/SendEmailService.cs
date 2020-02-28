2da70c2e06702375cfd45e2f558b84d8345b56c0
John Melville <jmelvill@hotmail.com>
2020-02-28 15:07:43 -0500

    retry email if it fails

--- Src/SendMailService/SendEmailService.cs
+++ Src/SendMailService/SendEmailService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Security;
+﻿using System;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
@@ -26,13 +27,31 @@ namespace SendMailService
       configuration.GetSection("Email").Bind(this);
      }
 
-    public Task SendEmail(string emailAddress, string subject, string htmlBody)
+    public async Task SendEmail(string emailAddress, string subject, string htmlBody)
+    {
+      // retry failed sending up to 5 times, with an increasing delay.
+      for (int i = 0; i < 5; i++)
+      {
+        try
+        {
+          await DoSend(emailAddress, subject, htmlBody);
+          return;
+        }
+        catch (Exception )
+        {
+          if (i == 4) throw;  /// preserve the final exception
+        }
+        await Task.Delay(TimeSpan.FromSeconds(1 + (2 * i)));
+      }
+    }
+
+    private Task DoSend(string emailAddress, string subject, string htmlBody)
     {
       log.Log(LogLevel.Information, $"Email sent to {emailAddress}: Subject");
       log.Log(LogLevel.Trace, $"Message Text: {htmlBody}");
       return Send(CreateMimeMessage(emailAddress, subject, htmlBody));
     }
-    
+
     private async Task Send(MimeMessage mimeMessage)
     {
       using var client = new SmtpClient
