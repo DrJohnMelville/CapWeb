d748283c942dc6863b75947f34a967b33aa9e87a
John Melville <jmelvill@hotmail.com>
2020-02-28 16:45:24 -0500

    title the email with the innermost exception for better sorting.

--- Src/TokenServiceClient.Website/MultiAuthenticationMiddleware.cs
+++ Src/TokenServiceClient.Website/MultiAuthenticationMiddleware.cs
@@ -59,51 +59,13 @@ namespace TokenServiceClient.Website
       {
         if (await handlers.GetHandlerAsync(context, scheme.Name) is IAuthenticationRequestHandler handler)
         {
-          if (await HandlerWithRefreshRetry(handler)) return true;
+          if (await handler.HandleRequestAsync()) return true;
         }
       }
 
       return false;
     }
-
-    private static async Task<bool> HandlerWithRefreshRetry(IAuthenticationRequestHandler handler)
-    {
-      return await HandleRefreshWithKeyRefresh(handler);
-    }
-
-    private static async Task<bool> HandleRefreshWithKeyRefresh(IAuthenticationRequestHandler handler)
-    {
-      try
-      {
-        return await handler.HandleRequestAsync();
-      }
-      catch (Exception e)
-      {
-        if (IsStaleKeyException(e) && handler is OpenIdConnectHandler openIdHandler)
-        {
-          RefreshStaleKeys(openIdHandler);
-          // we only try one refresh which is why this is not a recursive call
-          return await handler.HandleRequestAsync();
-        }
-        // otherwise
-        throw;
-      }
-    }
-
-    private static bool IsStaleKeyException(Exception e)
-    {
-      return e.Message.StartsWith("IDC10501");
-    }
-
-    private static void RefreshStaleKeys(OpenIdConnectHandler openIdHandler)
-    {
-      if (openIdHandler.Options.ConfigurationManager is ConfigurationManager<OpenIdConnectConfiguration> config)
-      {
-        config.RefreshInterval = TimeSpan.FromSeconds(1);
-      }
-      openIdHandler.Options.ConfigurationManager.RequestRefresh();
-    }
-
+    
     private void SetAuthenticationFeatures(HttpContext context)
     {
       context.Features.Set<IAuthenticationFeature>(new AuthenticationFeature
