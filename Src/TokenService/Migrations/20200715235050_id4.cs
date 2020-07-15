765073a9f3e3790bbe8d51787b5c5a375fcea791
John Melville <JohnMelville@gmail.com>
2020-07-15 19:53:27 -0400

    upgrade database

---
+++ Src/TokenService/Migrations/20200715235050_id4.cs
@@ -0,0 +1,41 @@
+﻿ using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TokenService.Migrations
+{
+    public partial class id4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConsumedTime",
+                table: "PersistedGrants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PersistedGrants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SessionId",
+                table: "PersistedGrants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsumedTime",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PersistedGrants");
+
+            migrationBuilder.DropColumn(
+                name: "SessionId",
+                table: "PersistedGrants");
+        }
+    }
+}
